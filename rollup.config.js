9ad260cfd93324a07a68c99c86f61d3807c84cd7
Paul C Roberts <pacaro@gmail.com>
2026-07-24 13:01:55 -0700

    Add umd supprt
    security update for dev dependency

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,19 @@ import terser from "@rollup/plugin-terser";
 
 export default {
   input: "./src/index.ts",
-  output: {
-    file: "./dist/index.js",
-    format: "esm",
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: "./dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "./dist/index.umd.js",
+      format: "umd",
+      sourcemap: true,
+      name: "PcPc",
+      exports: "named",
+    },
+  ],
   plugins: [typescript(), terser()],
 };
